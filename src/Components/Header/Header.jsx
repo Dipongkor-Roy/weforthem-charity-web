3866bfb9a4f316f12541e7d66970dac3408a2837
Dipongkor-Roy <juniordipu@gmail.com>
2023-12-10 03:43:42 +0600

    first commit

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom";
+
+
+const Header = () => {
+  return (
+    <>
+      <header className="text-gray-600 body-font">
+        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+          <span className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+          <Link to="/">  <p className="text-2xl text-purple-500">WeTorThem</p></Link>
+          </span>
+          <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
+           <Link to="/" ><a className="mr-5 hover:text-gray-900">Home</a></Link>
+            <a className="mr-5 hover:text-gray-900">Donation</a>
+            <a className="mr-5 hover:text-gray-900">Events</a>
+            <a className="mr-5 hover:text-gray-900">Blog</a>
+          </nav>
+         <Link to="/logIn"> <button className="inline-flex items-center text-white  bg-purple-500 border-0 py-2 px-4 mr-2 focus:outline-none hover:bg-purple-600 rounded-md text-base mt-4 md:mt-0">
+           LogIn
+          </button></Link>
+         
+         <Link to="/register"> <button className="inline-flex items-center text-white  bg-purple-500 border-0 py-2 px-4 focus:outline-none hover:bg-purple-600 rounded-md text-base mt-4 md:mt-0">
+            Register
+          </button></Link>
+        </div>
+      </header>
+    
+    </>
+  );
+};
+
+export default Header;
---
+++ src/Components/Routes/Routes.jsx
@@ -0,0 +1,38 @@
+import { createBrowserRouter } from "react-router-dom";
+import Hero from "../Hero/Hero";
+import LogIn from "../LogIn/LogIn";
+import Main from "../Main/Main";
+import Register from "../Register/Register";
+import Donation from "../Donation/Donation";
+import Event from "../Event/Event";
+
+
+export const router = createBrowserRouter([
+    { path:'/',
+    element:<Main></Main>,
+    children:[
+        {
+            path:"/",
+            element:<Hero/>
+        },
+        {
+            path:'/logIn',
+            element:<LogIn/>
+        },
+        {
+            path:"/register",
+            element:<Register/>
+        },
+        {
+            path:'/donation',
+            element:<Donation/>
+        },
+        {
+            path:"/events",
+            element:<Event/>
+        }
+        
+    ],
+
+
+    }])
