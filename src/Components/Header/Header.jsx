82f349e58968a72064d67d8762dd2ed4f54f5e32
Dipongkor-Roy <juniordipu@gmail.com>
2023-12-11 03:44:34 +0600

    navbar responsive work start

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,14 +5,16 @@ const Header = () => {
   return (
     <>
       <header className="text-gray-600 body-font">
+        {/* nav hidden*/}
+     
         <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
           <span className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
           <Link to="/">  <p className="text-2xl text-purple-500">WeTorThem</p></Link>
           </span>
           <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
            <Link to="/" ><a className="mr-5 hover:text-gray-900">Home</a></Link>
-            <a className="mr-5 hover:text-gray-900">Donation</a>
-            <a className="mr-5 hover:text-gray-900">Events</a>
+           <Link to="/donation" ><a className="mr-5 hover:text-gray-900">Donation</a></Link>
+         <Link to="/events"> <a className="mr-5 hover:text-gray-900">Events</a></Link>  
             <a className="mr-5 hover:text-gray-900">Blog</a>
           </nav>
          <Link to="/logIn"> <button className="inline-flex items-center text-white  bg-purple-500 border-0 py-2 px-4 mr-2 focus:outline-none hover:bg-purple-600 rounded-md text-base mt-4 md:mt-0">
--- src/Components/LogIn/LogIn.jsx
+++ src/Components/LogIn/LogIn.jsx
@@ -44,17 +44,6 @@ const LogIn = () => {
               Log In
             </h2>
             <div className="relative mb-4">
-              <label className="leading-7 text-sm text-gray-600">
-                Full Name
-              </label>
-              <input
-                type="text"
-                id="full-name"
-                name="full-name"
-                className="w-full bg-white rounded border border-gray-300 focus:border-purple-500 focus:ring-2 focus:ring-purple-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              />
-            </div>
-            <div className="relative mb-4">
               <label className="leading-7 text-sm text-gray-600">Email</label>
               <input
                 type="email"
